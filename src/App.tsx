53c9c76b2e3ed6a40be455af35444bba0ce14937
Your Name <you@example.com>
2025-10-17 10:24:19 +0200

    kesz

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,41 @@
-const App = () => {
+import { useEffect, useState } from "react";
+import Faq from "./components/FAQ.tsx";
+import star from "./assets/images/icon-star.svg"
+
+interface FaqElem {
+  question: string;
+  answer: string;
+}
+
+function App() {
+  const [FaqState, setFaqs] = useState<FaqElem[]>([]);
+  const [openIndex, setOpenIndex] = useState<number | null>(0);
+
+  useEffect(()=>{
+    fetch("./data.json")
+    .then((res) => res.json())
+    .then((data) => setFaqs(data))
+  }, []);
+
+  const ToggleFaq = (index: number) => {
+    setOpenIndex(openIndex === index ? null: index)
+  };
+
   return (
-    <main>
-    </main>
-  )
+    <section className="faq-container">
+        <img src={star} alt="star icon" className="faq-star" />
+        <h2 className="faq-title">FAQs</h2>
+        {FaqState.map((item, index) => (
+          <Faq 
+            key={index} 
+            question={item.question} 
+            answer={item.answer} 
+            isOpen={openIndex === index} 
+            onClick={() => ToggleFaq(index)} 
+          />
+        ))}
+    </section>
+  );
 }
 
 export default App
--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -1,9 +1,28 @@
-const FAQ = () => {
+import React from "react";
+import plus from "../assets/images/icon-plus.svg"
+import minus from "../assets/images/icon-minus.svg"
 
-  return (
-    <div className="faqCard">
-    </div>
-  )
+interface FaqProps {
+  question: string;
+  answer: string;
+  isOpen: Boolean;
+  onClick: () => void;
 }
 
-export default FAQ
+const Faq: React.FC<FaqProps> = ({ question, answer, isOpen, onClick }) => {
+  return (
+    <div className="faq-item">
+      <button className="faq-question" onClick={onClick} type="button">
+        <span>{question}</span>
+        <img 
+          src={isOpen ? plus : minus } 
+          alt={isOpen ? "Collapse" : "Expand"}
+          className="faq-icon" 
+        />
+        </button>
+        {isOpen && <p className="faq-answer">{answer}</p>}
+    </div>
+  );
+};
+
+export default Faq;
